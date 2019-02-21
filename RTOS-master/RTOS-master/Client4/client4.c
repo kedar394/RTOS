e501a2f68f9d274d5b04d41b44979eab8ce233b1
kedar394 <30826396+kedar394@users.noreply.github.com>
2019-02-21 17:51:32 +0530

    Update client4.c

--- RTOS-master/RTOS-master/Client4/client4.c
+++ RTOS-master/RTOS-master/Client4/client4.c
@@ -1,5 +1,4 @@
-#include<unistd.h>
-#include<stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h> 
@@ -8,7 +7,7 @@
 #include <time.h>
 
 struct msg_buffer 
-{ 
+{  
     int qid;
     long msg_type; 
     char msg_text[1024]; 
@@ -17,34 +16,34 @@ struct msg_buffer
 int main() 
 { 
     struct msg_buffer msg_rcv, msg_snd;
-    struct timeval start, end;
+    struct timeval start, finish;
     msg_rcv.msg_type=1;
     msg_snd.msg_type=1;
 
-    key_t ks,kr; 
+    key_t snd,rcv; 
 
-
-    ks = ftok("/home/kedar/iiitb/sem8/RTOS-master/RTOS-master/EchoEngine/server.c", 65); 
-    kr= ftok("client4",'4');
+    snd = ftok("/home/kedar/iiitb/sem8/RTOS-master/RTOS-master/EchoEngine/server.c", 65); 
     
+    rcv = ftok("client4",'4'); 
+
     int myid,sqid;
 
-
-    if((sqid= msgget(ks, 0666 | IPC_CREAT))==-1) 
+    sqid = msgget(snd, 0666 | IPC_CREAT);
+    if(sqid == -1) 
     {
         perror("server:msgget");
         exit(1);
     }
 
-
-    if((myid=msgget(kr, 0666|IPC_CREAT))==-1)
+    myid = msgget(rcv, 0666|IPC_CREAT);
+    if(myid == -1) 
     {
         perror("msgget");
         exit(1);
     }
     msg_snd.qid=myid;
 
-    while(fgets(msg_snd.msg_text, 198, stdin))
+    while(fgets(msg_snd.msg_text, 198, stdin)) 
     {
         gettimeofday(&start, NULL);
         if (msgsnd (sqid, &msg_snd, sizeof (struct msg_buffer), 0) == -1) 
@@ -52,32 +51,24 @@ int main()
             perror ("client: msgsnd");
             exit (1);
         }
-
-        printf("Recieving..\n");
-        if(msgrcv(myid, &msg_rcv, sizeof(struct msg_buffer), 0, 0)==-1)
+        
+        printf("Waiting for response..\n");
+        
+        if(msgrcv(myid, &msg_rcv, sizeof(struct msg_buffer), 0, 0) == -1)
         {
             perror("client:msgrcv");
             exit(1);
         }
+        gettimeofday(&finish, NULL);
 
-        gettimeofday(&end, NULL);
-
-        long seconds = (end.tv_sec - start.tv_sec);
-        long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
-
-        printf("Recieved: %s\n", msg_rcv.msg_text);
-        printf("%d seconds %d microseconds\n", seconds, micros);
-
-        printf ("Please type a message: ");
+        long seconds = (finish.tv_sec - start.tv_sec); 
+        long micros = ((seconds * 1000000) + finish.tv_usec) - (start.tv_usec);
+        printf("File content: %s\n", msg_rcv.msg_text);
+        
+        printf("%ld seconds %ld microseconds\n", seconds, micros);
+        
     }
 
-    if (msgctl (myid, IPC_RMID, NULL) == -1) 
-    {
-        perror ("client: msgctl");
-        exit (1);
-    }
-
-    printf ("Client: bye\n");
 
 
     if (msgctl (myid, IPC_RMID, NULL) == -1) 
@@ -85,11 +76,6 @@ int main()
         perror ("client: msgctl");
         exit (1);
     }
-
-    printf ("Client: bye\n");
-    exit(0);
-
-
 
     return 0; 
 }
