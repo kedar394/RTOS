fa42d0d6851900ea4d0b5e594e834d752d446f8f
kedar394 <30826396+kedar394@users.noreply.github.com>
2019-02-21 18:16:09 +0530

    Update server.c

--- RTOS-master/RTOS-master/EchoEngine/server.c
+++ RTOS-master/RTOS-master/EchoEngine/server.c
@@ -1,5 +1,4 @@
-#include<unistd.h>
-#include<stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h> 
@@ -13,40 +12,45 @@ struct msg_buffer
     char msg_text[1024]; 
 }; 
 
-int main(void)
+int main()
 {
     struct msg_buffer msg_rcv, msg_snd;
+
     key_t key;
-    int myQid;
-    //char dataToBeRead[1024];
-    if((key= ftok("/home/kedar/iiitb/sem8/RTOS-master/RTOS-master/EchoEngine/server.c", 65))==-1) 
+    int id;
+    
+    key = ftok("/home/kedar/iiitb/sem8/RTOS-master/RTOS-master/EchoEngine/server.c", 65);
+    if(key == -1)
     {
         perror("ftok");
         exit(1);
     }
     
-    if((myQid = msgget(key, 0666 | IPC_CREAT))==-1) 
+    id = msgget(key, 0666 | IPC_CREAT);
+    if(id == -1) 
     {
         perror("msgget");
         exit(1);
     }
     
-    printf("SERVER ID %d\n", myQid);
+    // printf("Sbrttgtebt %d\n", id);
+
     while(1)
     {
-        msgrcv(myQid, &msg_rcv, sizeof(struct msg_buffer), 0, 0); 
+        msgrcv(id, &msg_rcv, sizeof(struct msg_buffer), 0, 0); 
 
-        printf("Got: %s\n", msg_rcv.msg_text); //got message
+        printf("Got: %s\n", msg_rcv.msg_text);
 
-        if(msg_rcv.msg_text[0]<='z' && msg_rcv.msg_text[0]>='a')
-           msg_rcv.msg_text[0]= (char)(msg_rcv.msg_text[0]-32);
-        
+        if(msg_rcv.msg_text[0] >= 'a' && msg_rcv.msg_text[0] <= 'z')
+        {
+            msg_rcv.msg_text[0] = (char)(msg_rcv.msg_text[0] - 32); 
+        }  
         msg_snd=msg_rcv;
 
-        printf("Sending %s\n", msg_snd.msg_text); 
+        // printf("check send::: %s\n", msg_snd.msg_text); 
         int client_id=msg_rcv.qid;
 
-        if(msgsnd(client_id, &msg_snd, sizeof(struct msg_buffer),0)==-1) 
+        if(msgsnd(client_id, &msg_snd, sizeof(struct msg_buffer), 0) == -1) 
         {
             perror("msgsnd");
             exit(1);
